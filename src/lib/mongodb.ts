1d19c2424468adf2bd1b74538343463494aa82d4
shqipron01 <57071701+shqipron01@users.noreply.github.com>
2025-06-18 22:28:28 +0200

    Fix mongodb peer dependency for Vercel

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,13 +1,27 @@
 import { MongoClient } from "mongodb";
 
+const uri = process.env.MONGODB_URI as string;
+
+if (!uri) {
+  throw new Error("Please define the MONGODB_URI environment variable");
+}
+
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
 declare global {
   var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
-const uri = process.env.MONGODB_URI as string;
-const client = new MongoClient(uri);
-
-const clientPromise =
-  global._mongoClientPromise ?? (global._mongoClientPromise = client.connect());
+if (process.env.NODE_ENV === "development") {
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri);
+    global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise!;
+} else {
+  client = new MongoClient(uri);
+  clientPromise = client.connect();
+}
 
 export default clientPromise;
